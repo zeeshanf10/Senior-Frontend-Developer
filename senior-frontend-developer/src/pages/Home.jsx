c157bdf1f039a2ae2940428a9b90e5b5e71decb3
Zeeshan Zahoor <109585885+zeeshanf10@users.noreply.github.com>
2024-01-31 12:59:02 +0500

    clock task

--- senior-frontend-developer/src/pages/Home.jsx
+++ senior-frontend-developer/src/pages/Home.jsx
@@ -1,16 +1,41 @@
-import React from 'react'
-import '../App.css'
-import { Link } from 'react-router-dom'
+import React from "react";
+import "../App.css";
+import hour from "./hourTicks.png";
+import minutes from "./minutesTicks.png";
+import minutesPin from "./minutesPin.png";
+import hoursPin from "./hoursPin.png";
+import secondsPin from "./secondsPin.png";
+import pinStopper from "./pinStopper.png";
+
+
+
 function Home() {
   return (
     <>
-       
-
-    <div className='d-flex align-items-cnter justify-content-center fw-bold mt-5'>
-        Hello, this is home page
-    </div>
+      <div className="d-flex align-items-center justify-content-center mt-5">
+        <div className="clock position-relative">
+          <div>
+            <img src={hour} width="200px" alt="hours" />
+          </div>
+          <div className="minutes-ticks">
+            <img src={minutes} width="200px" alt="hours" />
+          </div>
+          <div className="minutes-pin">
+          <img src={minutesPin} width="80px" alt="hours" />
+          </div>
+          <div className="seconds-pin">
+          <img src={secondsPin} width="80px" alt="hours" />
+          </div>
+          <div className="hours-pin">
+          <img src={hoursPin} width="80px" alt="hours" />
+          </div>
+          <div className="pin-stopper">
+          <img src={pinStopper}  alt="hours" />
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
